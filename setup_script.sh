bd01ccb069982fc6287e009ad242f4443a81ed83
Laxman <48151266+yetulaxman@users.noreply.github.com>
2025-09-19 09:18:46 +0300

    Update setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -2,10 +2,10 @@ module load purge
 module load LUMI
 module load lumi-container-wrapper
 
-mkdir -p /projappl/project_462001071/$USER/pmx_workflow
-conda-containerize new --prefix /projappl/project_462001071/$USER/pmx_workflow  conda_env.yaml 
-export PATH="/projappl/project_462001071/$USER/pmx_workflow/bin:$PATH"
-mkdir -p /projappl/project_462001071/$USER/pmx_workflow/pmx_install
-cp post.sh /projappl/project_462001071/$USER/pmx_workflow/pmx_install
-cd /projappl/project_462001071/$USER/pmx_workflow/pmx_install
-conda-containerize update  /projappl/project_462001071/$USER/pmx_workflow  --post-install post.sh
+mkdir -p /projappl/project_xxxxx/$USER/pmx_workflow
+conda-containerize new --prefix /projappl/project_xxxxx/$USER/pmx_workflow  conda_env.yaml 
+export PATH="/projappl/project_xxxxx/$USER/pmx_workflow/bin:$PATH"
+mkdir -p /projappl/project_xxxxx/$USER/pmx_workflow/pmx_install
+cp post.sh /projappl/project_xxxxx/$USER/pmx_workflow/pmx_install
+cd /projappl/project_xxxxx/$USER/pmx_workflow/pmx_install
+conda-containerize update  /projappl/project_xxxxx/$USER/pmx_workflow  --post-install post.sh
